abcebb9be81ca207ee204dfd215f5ef2882dd943
KavMors <kavmors@163.com>
2024-08-26 20:27:54 +0800

    launch service on boot

--- app/src/main/java/com/kavmors/goplus/LaunchBaseActivity.java
+++ app/src/main/java/com/kavmors/goplus/LaunchBaseActivity.java
@@ -1,9 +1,7 @@
 package com.kavmors.goplus;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class LaunchBaseActivity extends Activity {
 
@@ -12,7 +10,5 @@ public class LaunchBaseActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         MonitorService.start(this);
-
-        Toast.makeText(getApplicationContext(), R.string.service_started, Toast.LENGTH_SHORT).show();
     }
 }
